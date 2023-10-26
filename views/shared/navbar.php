5d209ece207470794fbcf917a8eedbf9b97139cb
Damian Miela <damian.miela@gmail.com>
2023-10-26 18:08:50 +0200

    frontend fixes: added icons into navbar, fixed dropdown menu. Created Dashboard template to later fill with content

--- views/shared/navbar.php
+++ views/shared/navbar.php
@@ -1,11 +1,11 @@
 <nav id ="nav-bar">
-    <a href="/dashboard">Dashboard</a>
-    <a href="/myplans">My plans</a>
-    <a href="/makenewplan">Make a plan</a>
+    <a href="/dashboard"><img class="icon" src="../img/icons/dashboard.png" /><span class="textlink">Dashboard</span></a>
+    <a href="/myplans"><img class="icon" src="../img/icons/myplans.png" /><span class="textlink">My plans</span></a>
+    <a href="/makenewplan"><img class="icon" src="../img/icons/mnp.png" /><span class="textlink">Make a plan</span></a>
     
     <?php if (isset($_SESSION['username'])) : ?>
     <div class="dropdown">
-        <a class="dropbtn" href="/profile"><?php echo $_SESSION['username']?></a>
+        <a class="dropbtn" href="/profile"><img class="icon" src="../img/icons/user.png" /><span class="textlink userLink"><?php echo $_SESSION['username']?></span></a>
         <div class="dropdown-content">
            <!-- adding admin panel if the role of the user is admin -->
             <?php 
--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -5,8 +5,26 @@
 <div id="content">
    
     <h2>Dashboard</h2>
-    
-    
-    
+    <div class="grid">
+        <div class="two-thirds">
+            <div class="section">
+                <h3 class="center">My plans</h2>
+            </div>
+            <div class="section">
+                <h3 class="center">My progress</h2>
+            </div>
+        </div>
+
+        <div class="one-third">
+            <div class="section">
+                <h3 class="center">My profile</h2>
+            </div>
+        </div>
+        <div class="one-third">
+            <div class="section">
+                <h3 class="center">My friends</h2>
+            </div>
+        </div>
+    </div>
     
 </div>
